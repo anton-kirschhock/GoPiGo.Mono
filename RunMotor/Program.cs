f013d431c9c00aa138a05e3557a768d5802519ea
Anton Kirschhock <anton.kirschhock@student.ucll.be>
2016-11-25 22:43:34 +0100

    Simple Collision-avoidance

--- RunMotor/Program.cs
+++ RunMotor/Program.cs
@@ -18,16 +18,61 @@ namespace RunMotor
             var factory = DeviceFactory.Factory;
             using (var gopigo = factory.BuildGoPiGo())
             {
+                DateTime dtStart = DateTime.Now;
                 var ultrasonic = factory.BuildUltraSonicSensor(Pin.Analog1);
-                //var l1 = factory.BuildLed(Pin.LedLeft);
-                //var l2 = factory.BuildLed(Pin.LedRight);
-                //l1.ChangeState(GoPiGo.Sensors.SensorStatus.On);
-                //l2.ChangeState(GoPiGo.Sensors.SensorStatus.On);
-                //Thread.Sleep(2500);
+                gopigo.MotorController().EnableServo();
+                gopigo.MotorController().RotateServo(90);
+                gopigo.MotorController().DisableServo();
+                while ((DateTime.Now - dtStart).TotalMinutes < 5)
+                {
+                    Console.WriteLine("battery: " + gopigo.BatteryVoltage());
+                    gopigo.MotorController().Stop();
+                    gopigo.MotorController().MoveForward();
+                    Thread.Sleep(50);
+                    var d = ultrasonic.MeasureInCentimeters();
+                    while (d > 15)
+                    {
+                        Thread.Sleep(500);
+                        d = ultrasonic.MeasureInCentimeters();
+                        Console.WriteLine($"Driving forward with {d} cm of space");
 
-                //l1.ChangeState(GoPiGo.Sensors.SensorStatus.Off);
-                //l2.ChangeState(GoPiGo.Sensors.SensorStatus.Off);
 
+                    }
+                    gopigo.MotorController().Stop();
+                    
+                    while (d < 30)
+                    {
+                        gopigo.MotorController().RotateLeft();
+                        Thread.Sleep(500);
+                        gopigo.MotorController().Stop();
+                        Thread.Sleep(500);
+                        d = ultrasonic.MeasureInCentimeters();
+                        Thread.Sleep(10);
+                    }
+                }
+            }
+
+        }
+    }
+
+}
+/*
+                 var ultrasonic = factory.BuildUltraSonicSensor(Pin.Analog1);
+                var l1 = factory.BuildLed(Pin.LedLeft);
+                var l2 = factory.BuildLed(Pin.LedRight);
+                Thread.Sleep(2500);
+                l1.ChangeState(GoPiGo.Sensors.SensorStatus.On);
+                l2.ChangeState(GoPiGo.Sensors.SensorStatus.On);
+                Thread.Sleep(2500);
+
+                l1.ChangeState(GoPiGo.Sensors.SensorStatus.Off);
+                l2.ChangeState(GoPiGo.Sensors.SensorStatus.Off);
+                gopigo.MotorController().EnableServo();
+                for (int i = 0; i < 180; i++)
+                {
+                    gopigo.MotorController().RotateServo(i);
+                }
+                gopigo.MotorController().DisableServo();
                 Console.WriteLine($"Dist: {ultrasonic.MeasureInCentimeters()}cm");
                 Thread.Sleep(2500);
 
@@ -53,7 +98,4 @@ namespace RunMotor
                 Thread.Sleep(2500);
 
                 gopigo.MotorController().Stop();
-            }
-        }
-    }
-}
+     */
